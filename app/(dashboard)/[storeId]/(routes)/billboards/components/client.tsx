f614277fa6e22b11ea384d5a45dfbc0b4fa96843
SAFWAT <safwathkhan2@gmail.com>
2023-07-12 18:55:34 +0530

    data table added

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -6,26 +6,33 @@ import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+import { BillboardColumn, columns } from "./columns";
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
 
-    const params = useParams();
-    const router = useRouter();
-
+export const BillboardClient = ({ data }: BillboardClientProps) => {
+  const params = useParams();
+  const router = useRouter();
 
   return (
     <>
-        <div className="flex items-center justify-between">
-          <Heading
-            title="Billboard (0)"
-            description="Manage billboard for your store."
-          />
-          <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
-            <Plus className="w-4 h-4 mr-2" />
-            Add New
-          </Button>
-        </div>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboard (${data.length})`}
+          description="Manage billboard for your store."
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="w-4 h-4 mr-2" />
+          Add New
+        </Button>
+      </div>
       <Separator />
+      <DataTable searchKey="label" columns={columns} data={data}/>
     </>
   );
 };
